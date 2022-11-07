17630cc518f4ca59eb784c343bfc87130c8f4f0d
Ricky Hsu <rickyhsu0101@gmail.com>
2022-11-06 19:02:38 -0600

    used argparse

--- python/reader.py
+++ python/reader.py
@@ -1,9 +1,10 @@
-import sys
+import argparse, sys
 import signal
 import serial
 
 
 file_name = "collected"
+device_number = 0
 data = []
 def write_file():
     with open("../data/{}.csv".format(file_name), "w") as f:
@@ -15,16 +16,26 @@ def handler(signum, frame):
     write_file()
     exit(0)
 
-def main(v):
-    ser = serial.Serial('/dev/ttyACM{}'.format(v))
+def main():
+    ser = serial.Serial('/dev/ttyACM{}'.format(device_number))
     while(1):
         line = ser.readline()
         data.append(line)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        file_name = sys.argv[1]
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", help = "Unique device number in '/dev/ttyACM\{\}'")
+    parser.add_argument("--file", help="Output file name '\{\}.csv'")
+
+    args = parser.parse_args()
+
+    if args.device is not None:
+        device_number = args.device
+    if args.file is not None:
+        file_name = args.file
+
     signal.signal(signal.SIGINT, handler)
     main()
 
