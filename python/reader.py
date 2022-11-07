8ff710cf1ab431fa4296e63ab58fa73bd57bac60
Ricky Hsu <rickyhsu0101@gmail.com>
2022-11-06 20:11:52 -0600

    updated script

--- python/reader.py
+++ python/reader.py
@@ -6,10 +6,14 @@ import serial
 file_name = "collected"
 device_number = 0
 data = []
+
+DEVICE_MAC = "/dev/tty.usbmodem"
+DEVICE_LINUX = "/dev/ttyACM"
+
+os = DEVICE_LINUX
 def write_file():
-    with open("../data/{}.csv".format(file_name), "w") as f:
-        for line in data:
-            f.write(line + "\n")
+    with open("../data/{}-{}.csv".format(file_name, device_number), "w") as f:
+        f.write(''.join(data))
         f.close()
 
 def handler(signum, frame):
@@ -17,10 +21,11 @@ def handler(signum, frame):
     exit(0)
 
 def main():
-    ser = serial.Serial('/dev/ttyACM{}'.format(device_number))
+    ser = serial.Serial('{}{}'.format(os, device_number))
     while(1):
         line = ser.readline()
-        data.append(line)
+        print(line)
+        data.append(line.decode("utf-8"))
 
 
 if __name__ == "__main__":
@@ -28,6 +33,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--device", help = "Unique device number in '/dev/ttyACM\{\}'")
     parser.add_argument("--file", help="Output file name '\{\}.csv'")
+    parser.add_argument("--os", help="l for linux, m for mac")
 
     args = parser.parse_args()
 
@@ -35,7 +41,11 @@ if __name__ == "__main__":
         device_number = args.device
     if args.file is not None:
         file_name = args.file
-
+    if args.os is not None and args.os == "m":
+        os = DEVICE_MAC
+    print(device_number)
+    print(file_name)
+    print(os)
     signal.signal(signal.SIGINT, handler)
     main()
 
